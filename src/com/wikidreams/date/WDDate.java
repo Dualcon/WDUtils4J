92f989d06d1028ace418a346f5433e1b881759b1
WikiDreams <danielsantosvieiramail@gmail.com>
2016-04-27 10:05:09 +0100

    update

---
+++ src/com/wikidreams/date/WDDate.java
@@ -0,0 +1,15 @@
+package com.wikidreams.date;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WDDate {
+
+	public static String getSystemDate() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		return dateFormat.format(date);
+	}
+
+}
